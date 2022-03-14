5c8184fd84ac510a6f3ead22652c2760761c28e0
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-03-14 11:11:50 +0100

    Added button to increase pass length and pass gen is done

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
-let password: string = ""
-let chars = []
+const chars: string[] = [];
 for (let i = 32; i < 127; i++) {
     chars.push(String.fromCharCode(i));
 }
 console.log(chars);
-let length = 8
-for (let j = 0; j <= length - 1; j++) {
-    console.log(j);
-const char = chars.get(Math.randomRange(0, chars.length))
-console.log(char);
-password = "" + password + char
-}
-console.log(password);
-basic.showString(password)
+let length = 8;
+basic.showString(length.toString());
+input.onButtonPressed(Button.B, function() {
+    length += 1;
+    basic.showString(length.toString());
+})
+input.onButtonPressed(Button.A, function() {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const char = chars.get(Math.randomRange(0, chars.length))
+        password += char;
+    }
+
+    console.log(password);
+    basic.showString(password);
+})
