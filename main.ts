2770b78699a5857dece5755f226fd45b54c7a83a
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-03-15 10:24:57 +0100

    Fixed generator

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ const chars: string[] = [];
 for (let i = 32; i < 127; i++) {
     chars.push(String.fromCharCode(i));
 }
+for (let i = 0; i < 9; i++) {
+    chars.push(i.toString());
+}
 console.log(chars);
 let length = 8;
 basic.showString(length.toString());
@@ -10,12 +13,17 @@ input.onButtonPressed(Button.B, function() {
     basic.showString(length.toString());
 })
 input.onButtonPressed(Button.A, function() {
+    if (length != 8) {
+        length -= 1;
+        basic.showString(length.toString());
+    }
+})
+input.onButtonPressed(Button.AB, function() {
     let password = "";
     for (let i = 0; i < length; i++) {
         const char = chars.get(Math.randomRange(0, chars.length))
         password += char;
     }
-
     console.log(password);
     basic.showString(password);
 })
